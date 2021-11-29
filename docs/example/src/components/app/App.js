b4fbc825793ecd2989eb8038a5d11a2c74d954a4
RubenVF <rubenvelazquez244@gmail.com>
2021-11-29 14:43:41 -0600

    Code block add in min.js

--- docs/example/src/components/app/App.js
+++ docs/example/src/components/app/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import Post from '../post/Post';
+import Output from '../../../../../index';
 import './App.css';
-const data = require('./testData');
+const data ={
+  "time" : 1638146432885,
+  "blocks" : [
+      {
+          "id" : "zqZd0lg-5j",
+          "type" : "code",
+          "data" : {"code" : "sadfsafsaxd"}
+      },
+      {
+          "id" : "az5PnFJ67R",
+          "type" : "header",
+          "data" : {
+              "text" : "Editor.js",
+              "level" : 2
+          }
+      }
+  ],
+  "version" : "2.22.2"
+}
 
 const App = () => (
   <div className='App'>
     <header>Editor.js React Renderer</header>
-    <Post data={ data } />
+    <Output data={data}/>
   </div>
 );
 
